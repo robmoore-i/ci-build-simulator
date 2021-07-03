156aaaefabb174b5d1c0321bbdd9e525cf189419
Rob Moore <rmoore@gradle.com>
2021-07-03 21:47:02 +0800

    Jenkinsfile credentials

--- app/Jenkinsfile.groovy
+++ app/Jenkinsfile.groovy
@@ -12,9 +12,13 @@ pipeline {
         stage("Extend test suite") {
             steps {
                 sh("./gradlew :app:generateStableTest")
-                sh("git commit -am'Extended test suite'")
                 echo("Git URL is $GIT_URL")
                 echo("Credentials: $GITHUB_CREDENTIALS")
+                sh("git config --global user.email \"jenkins@email.com\"")
+                sh("git config --global user.name \"Jenkins\"")
+                sh("git config --global github.user $GITHUB_CREDENTIALS_USR")
+                sh("git config --global github.token $GITHUB_CREDENTIALS_PSW")
+                sh("git commit -am'Extended test suite'")
             }
         }
     }
