5139d7433ff823a2ea7a5cf5a6251bb40aa863e2
Rob Moore <rmoore@gradle.com>
2021-07-03 22:21:55 +0800

    Jenkins push test suite extension

--- app/Jenkinsfile.groovy
+++ app/Jenkinsfile.groovy
@@ -1,30 +1,23 @@
-pipeline {
-    agent any
+node {
     environment {
         GITHUB_CREDENTIALS = credentials('GitHubPushAccess')
-        BRANCH = "$GIT_BRANCH".drop("origin/".length())
     }
-    stages {
-        stage("Run tests") {
-            steps {
-                sh("./gradlew :app:test")
-            }
-        }
-        stage("Extend test suite") {
-            steps {
-                sh("git checkout $BRANCH")
-                sh("git clean -fd")
-                sh("./gradlew :app:generateStableTest")
-                echo("Git URL is $GIT_URL")
-                echo("Credentials: $GITHUB_CREDENTIALS")
-                sh("git config --global user.email \"jenkins@email.com\"")
-                sh("git config --global user.name \"Jenkins\"")
-                sh("git config --global github.user $GITHUB_CREDENTIALS_USR")
-                sh("git config --global github.token $GITHUB_CREDENTIALS_PSW")
-                sh("git add app")
-                sh("git commit -am \"Extended test suite\"")
-                sh("git push")
-            }
-        }
+    stage("Run tests") {
+        sh("./gradlew :app:test")
+    }
+    stage("Extend test suite") {
+        def BRANCH = "$GIT_BRANCH".drop("origin/".length())
+        sh("git checkout $BRANCH")
+        sh("git clean -fd")
+        sh("./gradlew :app:generateStableTest")
+        echo("Git URL is $GIT_URL")
+        echo("Credentials: $GITHUB_CREDENTIALS")
+        sh("git config --global user.email \"jenkins@email.com\"")
+        sh("git config --global user.name \"Jenkins\"")
+        sh("git config --global github.user $GITHUB_CREDENTIALS_USR")
+        sh("git config --global github.token $GITHUB_CREDENTIALS_PSW")
+        sh("git add app")
+        sh("git commit -am \"Extended test suite\"")
+        sh("git push")
     }
 }
