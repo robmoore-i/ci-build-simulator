91db1ba9f76f4dd2c64d9267ce359b8140b4ecac
Rob Moore <rmoore@gradle.com>
2021-07-03 21:28:35 +0800

    Simplify Jenkinsfile

--- app/Jenkinsfile.groovy
+++ app/Jenkinsfile.groovy
@@ -2,13 +2,6 @@ pipeline {
     agent any
 
     stages {
-        stage("Checkout") {
-            steps {
-                sh("git --version")
-                checkout scm
-                sh("ls")
-            }
-        }
         stage("Run tests") {
             steps {
                 sh("./gradlew :app:test")
