ce1bc2f12cbf805317af142d4b627ca5eff34d45
Rob Moore <rmoore@gradle.com>
2021-07-04 10:01:57 +0800

    Get Git branch in Jenkins

--- app/Jenkinsfile.groovy
+++ app/Jenkinsfile.groovy
@@ -2,8 +2,8 @@ node {
     stage("Setup") {
         sh("ls")
         sh("git --version")
-        def scmRefspec = scm.getUserRemoteConfigs()[0].getRefspec()
-        echo("Refspec: $scmRefspec")
+        def remoteConfig = scm.getUserRemoteConfigs()[0]
+        echo("RemoteConfig: $remoteConfig")
         String gitBranch = "main"
         sh("git checkout $gitBranch")
         sh("git reset --hard origin/$gitBranch")
